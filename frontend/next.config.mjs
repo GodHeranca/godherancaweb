e7425d9fa7b3ce6eb53b1cf47f24ae6106fa81eb
Olawale Olafisoye <walex.world20@gmail.com>
2024-08-11 09:22:54 -0300

    fixed env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_GOOGLE_API_KEY: 'AIzaSyASXMx9-yGdgyJNv0gNI8nUdaoRxlkRU1A',
-  },
 };
 
 export default nextConfig;
