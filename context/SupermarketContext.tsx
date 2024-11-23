9e7e66b479fa8bf1458925744e81759503656490
Olawale Olafisoye <walex.world20@gmail.com>
2024-11-23 09:05:07 -0300

    creat new folder

--- context/SupermarketContext.tsx
+++ context/SupermarketContext.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an instance of axios with a base URL
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
 // Interfaces matching your Mongoose models
